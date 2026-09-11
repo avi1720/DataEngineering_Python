8949fac0602087147440e62c6daf055cb2c514d0
Shrutvik <avinaashm1720@gmail.com>
2026-09-11 21:02:18 +0530

    11th September 2026

---
+++ 001_P2_Datatypes.py
@@ -0,0 +1,46 @@
+#Date: 11th September 2026
+#Topics Coverd: User input
+
+''' INTRODUCTION TO DATA TYPES
+
+CREATE ONE VARIABLE EACH FOR INT, FLOAT, STR, AND BOOL.
+PRINT THE VALUE AND DATA TYPE OF EACH VARIABLE USING TYPE().
+WHAT WILL BE THE DATA TYPE OF:
+    • 25
+    • 25.5
+    • "25"
+    • TRUE
+CREATE A VARIABLE CONTAINING "100" AND CHECK ITS DATA TYPE.
+CREATE A VARIABLE CONTAINING 100 AND CHECK ITS DATA TYPE.
+CREATE VARIABLES REPRESENTING YOUR NAME, AGE, PERCENTAGE, AND WHETHER YOU ARE A STUDENT. PRINT THEIR DATA TYPES. '''
+
+a = 25
+b = 25.5
+c = "HELLO"
+d = True
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+
+print(type(25))
+print(type(25.5))
+print(type("25"))
+print(type(True))
+
+x = "100"
+print(type(x))
+
+y = 100
+print(type(y))
+
+name = "AVINASH"
+age = 25
+percentage = 85.5
+student = True
+
+print(type(name))
+print(type(age))
+print(type(percentage))
+print(type(student))
